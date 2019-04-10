c947580e5b310bf44533b278b571f6a508f6d0f4
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2019-04-09 21:31:47 -0500

    [phpbb-extensions] Add Highlight.js extension

---
+++ phpbb-extensions/highlightjs.js
@@ -0,0 +1,10 @@
+{
+	"unstable": {
+		"0.1": {
+			"current": "0.1.0-dev",
+			"announcement": "https://github.com/AlfredoRamos/phpbb-ext-highlightjs",
+			"eol": null,
+			"security": false
+		}
+	}
+}
