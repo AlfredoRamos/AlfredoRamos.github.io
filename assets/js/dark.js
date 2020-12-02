d9afdb54d6480538bdba5cfda56abfa1a44ab308
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2020-12-02 04:45:04 -0600

    Update to Bootstrap v5

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -3,23 +3,23 @@
 
 	const config = {
 		attributeName: 'dark-mode',
-		iconClass: '.dark-mode-icon',
+		triggerClass: '.dark-mode-switch',
 		storeKey: 'dark_mode'
 	};
 
 	document.body.addEventListener('click', function(e) {
-		const icon = e.target.closest(config.iconClass);
+		const trigger = e.target.closest(config.triggerClass);
 
-		if (!icon) {
+		if (!trigger) {
 			return;
 		}
 
 		document.body.toggleAttribute(config.attributeName);
 		const theme = document.body.hasAttribute(config.attributeName) ? 1 : 0;
-
 		window.localStorage.setItem(config.storeKey, theme);
 	});
 
+	const triggerSwitch = document.body.querySelector(config.triggerClass + ' > .form-check-input');
 	const currentTheme = parseInt(window.localStorage.getItem(config.storeKey), 10);
 
 	if (isNaN(currentTheme)) {
@@ -29,15 +29,31 @@
 			if (!document.body.hasAttribute(config.attributeName)) {
 				document.body.setAttribute(config.attributeName, true);
 			}
+
+			if (triggerSwitch) {
+				triggerSwitch.checked = true;
+			}
 		} else {
 			if (document.body.hasAttribute(config.attributeName)) {
 				document.body.removeAttribute(config.attributeName);
+			}
+
+			if (triggerSwitch) {
+				triggerSwitch.checked = false;
 			}
 		}
 
 	} else if (currentTheme === 1 && !document.body.hasAttribute(config.attributeName)) {
 		document.body.setAttribute(config.attributeName, true);
+
+		if (triggerSwitch) {
+			triggerSwitch.checked = true;
+		}
 	} else if (currentTheme !== 1 && document.body.hasAttribute(config.attributeName)) {
 		document.body.removeAttribute(config.attributeName);
+
+		if (triggerSwitch) {
+			triggerSwitch.checked = false;
+		}
 	}
 })();
